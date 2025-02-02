f439588a1ccdd0824ee17b5c1d86bc99b4f0cae4
Rin-t <rin.t.4921@gmail.com>
2025-02-02 22:05:48 +0900

    Initial Commit

---
+++ SampleMVI/SampleMVIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleMVIApp.swift
+//  SampleMVI
+//
+//  Created by Rin on 2025/02/02.
+//
+
+import SwiftUI
+
+@main
+struct SampleMVIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SampleMVITests/SampleMVITests.swift
@@ -0,0 +1,16 @@
+//
+//  SampleMVITests.swift
+//  SampleMVITests
+//
+//  Created by Rin on 2025/02/02.
+//
+
+import Testing
+
+struct SampleMVITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
